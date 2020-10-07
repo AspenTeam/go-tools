c97e74890772dd4d6b73dcee9d88317b9fb8be15
David Barshow <david@datalamb.com>
2020-10-07 12:52:21 -0700

    updated update script

--- updateFromUpstream.sh
+++ updateFromUpstream.sh
@@ -2,5 +2,7 @@
 git remote add upstream https://github.com/dominikh/go-tools
 git fetch upstream
 git reset --hard upstream/master
-git checkout origin/master -- updateFromUpstream.sh
+git checkout origin/updateScript -- updateFromUpstream.sh
 find . -name '*.go' -type f -exec sed -i '' -e 's/honnef.co\/go\/tools/github\.com\/AspenTeam\/go-tools/g' {} \;q
+find . -name 'go.mod' -type f -exec sed -i '' -e 's/honnef.co\/go\/tools/github\.com\/AspenTeam\/go-tools/g' {} \;q
+git commit -a -m "Update from upstream re-ref to github"
