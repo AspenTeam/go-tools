5cabaf568c5451f76acd16586955914773b2e394
David Barshow <david@datalamb.com>
2020-10-07 13:08:20 -0700

    correct branch

--- updateFromUpstream.sh
+++ updateFromUpstream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git remote add upstream https://github.com/dominikh/go-tools
 git fetch upstream
-git reset --hard upstream/master
+git reset --hard upstream/release.2020.1
 git checkout origin/updateScript -- updateFromUpstream.sh
 find . -name '*.go' -type f -exec sed -i '' -e 's/honnef.co\/go\/tools/github\.com\/AspenTeam\/go-tools/g' {} \;q
 find . -name 'go.mod' -type f -exec sed -i '' -e 's/honnef.co\/go\/tools/github\.com\/AspenTeam\/go-tools/g' {} \;q
